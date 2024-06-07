6d0fcd3697604c24ae6eccb33b623d61d23aecb2
orwel009 <orwelvincent1@gmail.com>
2024-06-07 12:53:14 +0530

    Search Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBook from './components/AddBook';
+import Search from './components/Search';
 
 function App() {
   return (
     <div>
       <AddBook/>
+      <Search/>
     </div>
   );
 }
