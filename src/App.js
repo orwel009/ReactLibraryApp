aafaf8d4b4b792d3572925686fca66eea2bddfd1
orwel009 <orwelvincent1@gmail.com>
2024-06-07 12:55:54 +0530

    Delete  Added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddBook from './components/AddBook';
 import Search from './components/Search';
+import Delete from './components/Delete';
 
 function App() {
   return (
     <div>
       <AddBook/>
       <Search/>
+      <Delete/>
     </div>
   );
 }
